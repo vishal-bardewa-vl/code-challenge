d2ada8eaf5063decd567c7812034195befc41d37
vishal-bardewa-vl <vishal.bardewa@valuelabs.com>
2025-01-15 07:30:19 +0000

    task-3

--- project/packages/ui/components/List.tsx
+++ project/packages/ui/components/List.tsx
@@ -1,5 +1,23 @@
-import React from 'react'
+import './list.scss';
 
-export const List = () => {
-  return <p>List</p>
+const Card = ({name}: {name: string}) => {
+  return <div>
+    <p>{name}</p>
+  </div>
+  
 }
+
+export const List = ({ list }: any) => {
+  return (
+    <div >
+      <p>List</p>
+      {list?.length > 0 && (
+        <ul className="list">
+          {list.map((item: any) => (
+            <li className="list-item" key={item.name}><Card name={item.name} /></li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};

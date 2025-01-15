5e26a8f8e763141803244b65f2790e0f7529f0cd
vishal-bardewa-vl <vishal.bardewa@valuelabs.com>
2025-01-15 07:28:32 +0000

    task-1 and task-2

---
+++ Questions.md
@@ -0,0 +1,7 @@
+Question 1: How did you manage to fetch the list and what tool did you use?
+Ans: I made use of the fetch method to execute a get response. Other way would be to use axios to make the API call
+
+
+Question 2: What steps would you take to future improve this?
+Ans: Would use the axios package to make sure the the API call is made through it. Also define ENV variables to seperate DEV, STAGE and PROD endpoint urls
+
--- project/apps/app/src/App.tsx
+++ project/apps/app/src/App.tsx
@@ -1,12 +1,19 @@
+import { useEffect, useState } from 'react'
 import { List } from 'ui'
 
 const api = "https://pokeapi.co/api/v2/pokemon?limit=151"
 
 const App = () => {
+  const [state, setState] = useState()
+  useEffect(() => {
+    fetch(api).then(res => res.json()).then((res) => {
+      setState(res.results)})
+  }, [])
+  
   return (
   <>
     <h1>Pokemon list:</h1>
-    <List />
+    <List list={state} />
   </>
   )
 }
